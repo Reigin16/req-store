fe4b7cb17294c4e8d14d0c1b09944cd2fda2a00f
Hieser21 <rarafat883@proton.me>
2023-12-21 07:10:01 +0000

    Test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const store = require('store');
+const fs = require('fs')
 const update = require('store/plugins/update');
 const localStorage = require('store/storages/localStorage')
 const app = express();
@@ -8,12 +9,15 @@ store.addPlugin(update)
 store.createStore(localStorage)
 app.post('/', (req, res) => {
     let {data, updatedAt} = req.body
-    store.set('data', {data, updatedAt})
+    store.set('data', {data: data, updatedAt: updatedAt})
+
+    fs.writeFileSync('data.json', JSON.stringify(store.get('data')));
+
     res.status(200).json({message: "success", updatedAt: updatedAt})
     console.log("Updated at", updatedAt )
 })
 app.get('/', (req, res) => {
-    let output = store.get('data')
+    let output = JSON.parse(fs.readFileSync('data.json'));
     res.status(200).json(output)
 })
 app.listen(3000)
