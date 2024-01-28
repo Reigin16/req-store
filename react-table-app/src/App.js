d17ca64d13bf16b1eeb599eb8d57969fe6ed879d
Reigin16 Nasser <rarafat15@yahoo.com>
2024-01-28 06:51:16 +0000

    WIP Push

--- react-table-app/src/App.js
+++ react-table-app/src/App.js
@@ -4,14 +4,23 @@ import {useState, useEffect} from 'react'
 
 
 function Input(props) {
-  let [maxTime, setMaxTime] = useState("2")
+  let [maxTime, setMaxTime] = useState("")
  let index = props.index
-    let submitText = (e) => {
+    let submitText = async(e) => {
       e.preventDefault()
       var newData = {...props.data}
       console.log(props, maxTime)
-      newData[index].maxTime = maxTime
-      props.setData(newData)
+      newData[index] = {...newData[index], maxTime: maxTime}
+      console.log(props.data)
+      let data = newData
+          let response = await fetch('https://8000-hieser21-reqstore-4fxk1tod0g7.ws-us107.gitpod.io/data', {
+            headers: {'Content-Type': "application/json"},
+            method: 'POST',
+            body: JSON.stringify({data: [data]})
+          });
+          let result = await response.json();
+          console.log(result);
+        
     }
     
     return (
@@ -27,18 +36,29 @@ function App() {
   let [data, setData] = useState([])
 
   useEffect(() => {
-    async function dataFetch() {
-    if (data == '') {
-      let oldData = await fetch('https://8000-hieser21-reqstore-4fxk1tod0g7.ws-us107.gitpod.io/data').then(res => res.json())
-      console.log(oldData.data.data)
-      setData(oldData.data.data)
-    } else {
-      let res = await fetch('https://8000-hieser21-reqstore-4fxk1tod0g7.ws-us107.gitpod.io/data', {headers: {'Content-Type': "application/json"}, method: 'POST', body: JSON.stringify({data})}).then(res => res.json())
-      console.log(res)
+    async function fetchInitialData() {
+      let response = await fetch('https://8000-hieser21-reqstore-4fxk1tod0g7.ws-us107.gitpod.io/');
+      let oldData = await response.json();
+      setData(oldData);
     }
-  } 
-    dataFetch()
-  }, [data])
+    fetchInitialData();
+  }, []); // Empty dependency array to run only once
+
+  // Separate effect for posting data updates
+  // useEffect(() => {
+  //   async function postData() {
+  //     if (data.length > 0) { // Check if there's data to post
+  //       let response = await fetch('https://8000-hieser21-reqstore-4fxk1tod0g7.ws-us107.gitpod.io/data', {
+  //         headers: {'Content-Type': "application/json"},
+  //         method: 'POST',
+  //         body: JSON.stringify({data})
+  //       });
+  //       let result = await response.json();
+  //       console.log(result);
+  //     }
+  //   }
+  //   postData();
+  // }, [data]); // This will run every time `data` changes
   return (
     <div className="App">
     <h1>Students Table</h1>
@@ -53,18 +73,18 @@ function App() {
         </tr>
       </thead>
       <tbody>
-        {data.map((value, key) => {
+        {data.length > 0 ? data.map((value, key) => {
           
           return (
             <tr key={key}>
               <td>{value?.id}</td>
-              <td>{value?.title}</td>
+              <td>{value?.Title}</td>
               <td>{value?.status}</td>
               <td>{value?.lastRun}</td>
-              <td>{value?.maxTime !== null ? value.maxTime : (<Input index={key} data={data}  setData={setData} /> )}</td>
+              <td>{value?.maxTime == null ? (<Input index={key} data={data} setData={setData} />) : (<p>{value.maxTime}</p>)}</td>
             </tr>
           );
-        })}
+        }) : null}
       </tbody>
     </table>
   </div>
